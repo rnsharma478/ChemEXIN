3380b205dd49698ad76ff6a0dc9f1d83e22ae110
Dinesh Sharma <48877090+rnsharma478@users.noreply.github.com>
2024-05-02 12:05:21 +0530

    Update combine_tetra.py

--- src/combine_tetra.py
+++ src/combine_tetra.py
@@ -1,5 +1,5 @@
 '''
-This program file is part of "ChemEXIN: A Physicochemical Parameter-Based Exon-Intron Boundary Prediction Method" developed by the Computational Genomics group at Supercomputing Facility for Bioinformatics and Computational Biology (SCFBio), Kusuma School of Biological Sciences (KSBS), Indian Institute of Technology (IIT) Delhi, India.
+This program file is part of "Exon-Intron Boundary Detection Made Easy by Physicochemical Properties of DNA" developed by the Computational Genomics group at Supercomputing Facility for Bioinformatics and Computational Biology (SCFBio), Kusuma School of Biological Sciences (KSBS), Indian Institute of Technology (IIT) Delhi, India.
 Authors: Dinesh Sharma, Danish Aslam, Kopal Sharma, Aditya Mittal, B Jayaram.
 
 Contact: run478@gmail.com, bjayaram@chemistry.iitd.ac.in  
