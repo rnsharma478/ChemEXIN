4be0ad71d39adf4f60e53d9c143941523f2725b1
Dinesh Sharma <48877090+rnsharma478@users.noreply.github.com>
2024-05-02 12:09:59 +0530

    Update preprocess.py

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,6 +1,11 @@
+'''
+This program file is part of "Exon-Intron Boundary Detection Made Easy by Physicochemical Properties of DNA" developed by the Computational Genomics group at Supercomputing Facility for Bioinformatics and Computational Biology (SCFBio), Kusuma School of Biological Sciences (KSBS), Indian Institute of Technology (IIT) Delhi, India.
+Authors: Dinesh Sharma, Danish Aslam, Kopal Sharma, Aditya Mittal, B Jayaram.
 
+Contact: run478@gmail.com, bjayaram@chemistry.iitd.ac.in
 
-
+Lab Webpage: www.scfbio-iitd.res.in
+'''
 
 import sys
 import simple_colors as SC
